12029d425dd277a0074854365354f65333c69123
omaradesida <o.adesida@warwick.ac.uk>
2021-10-13 18:45:10 +0100

    restarts work properly this time

--- hs_alkane_NS.py
+++ hs_alkane_NS.py
@@ -30,32 +30,6 @@ time = args.time
 
 
 
-
-    
-print("importing stuff")
-
-#parsing inputs
-
-parent_dir = os.getcwd()
-parent_dir = "." #temporary until I rebuild hs_alkane to take longer filenames
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--iterations", default = 2e5, type=float, help = "Number of iterations to run \n")
-parser.add_argument("-w", "--nwalkers", default = 500, type=int, help = "Number of walkers to use \n")
-parser.add_argument("-c", "--nchains", default = 32, type=int, help = "Number of chains in the simulation box \n")
-parser.add_argument("-b", "--nbeads", default = 1, type=int, help = "Number of beads in each chain \n")
-parser.add_argument("-l", "--walklength", default = 80, type=int, help = "Number of sweep for each random walk step \n")
-parser.add_argument("-R", "--restart", action = "store_true", help = "Whether or not to restart from a previous attempt\n")
-parser.add_argument("-f","--restart_folder",type=str, help = 
-                    "Configurations file to restart from. Energies and Trajectory file should have the same file naming \
-                    convention. \n i.e if restart file is 'foo.restart' energies and trajectory file \
-                    should be foo.energies and foo.extxyz")
-
-args = parser.parse_args()
-from_restart = args.restart
-ns_iterations = int(args.iterations)
-
-
 if not from_restart:
     nwalkers = args.nwalkers
     nchains = args.nchains
@@ -239,7 +213,9 @@ for i in range(ns_iterations):
                      comment = celltoxmolstring(config))
         t1 = timer()
         t_elapsed = t1-t0
-        if t1-t_elapsed < 600:
+        if time-t_elapsed < 600:
+
+            print("Out of allocated time, writing to file and exiting")
             sys.exit()
 
 
